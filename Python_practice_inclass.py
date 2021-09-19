9b9d84596eadb951040e239680d97d1d5f5e864a
Ben Mogil <bmogil.hba2021@ivey.ca>
2021-09-19 16:37:54 -0400

    Finally completed asynchronous module

--- Python_practice_inclass.py
+++
@@ -1,7 +0,0 @@
-name = "Ben"
-Country = "Canada"
-age = 22
-hourly_wage = 21
-satisfied = True
-daily_wage = hourly_wage * 8
-print("My name is " + name + "")
--- rock_paper_scissors.py
+++
@@ -1,7 +0,0 @@
-import random
-
-our_List = ['r', 'p', 's']
-print(random.choice(our_List))
-the_User = input("Enter r, p, or s")
-if random.choice(our_List) == the_User:
-    print("It's a tie!")
